49f982d63cacfc1f0ee21bfff35b78cf926df30c
rdrayhan7916 <rdrayhan7916@gmail.com>
2021-11-12 00:53:04 +0600

    post

--- index.js
+++ index.js
@@ -18,6 +18,8 @@ async function run() {
         await client.connect()
         const database = client.db('alica-point')
         const productsCollection = database.collection('products')
+        const usersCollection = database.collection('users');
+        const reviewCollection = database.collection('review')
 
 
         app.get('/products', async (req, res) => {
@@ -25,6 +27,33 @@ async function run() {
             const services = await cursor.toArray();
             res.send(services)
         })
+
+        app.get('/review', async (req, res) => {
+            const cursor = reviewCollection.find({});
+            const review = await cursor.toArray();
+            res.send(review)
+        })
+
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const result = await usersCollection.insertOne(user);
+            console.log(result);
+            res.json(result);
+        });
+        app.put('/users', async (req, res) => {
+            const user = req.body;
+            const filter = { email: user.email };
+            const options = { upsert: true };
+            const updateDoc = { $set: user };
+            const result = await usersCollection.updateOne(filter, updateDoc, options);
+            res.json(result);
+        });
+        app.post('/addreview', async (req, res) => {
+            const user = req.body;
+            const result = await reviewCollection.insertOne(user);
+            console.log(result);
+            res.json(result);
+        });
     }
     finally {
         // await client.close();
